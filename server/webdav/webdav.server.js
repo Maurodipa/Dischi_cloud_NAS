522906e4a3247b6ccbc6cea06e06c72e457f47cf
Maurodipa <maurodipa@github.com>
2026-09-16 16:16:19 -0600

    fix: grant authenticated users access to WebDAV root path to allow RaiDrive connection

--- server/webdav/webdav.server.js
+++ server/webdav/webdav.server.js
@@ -86,6 +86,10 @@ async function startWebDAVServer() {
       server.beforeRequest((ctx, next) => {
         if (ctx.user) {
           const username = ctx.user.username;
+          
+          // Grant access to the root folder so the client can connect and list contents
+          privilegeManager.setRights(ctx.user, '/', ['all']);
+          
           const userPath = '/' + username;
           
           if (!mountedUsers.has(username)) {
